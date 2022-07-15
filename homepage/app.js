c82edfcf83de6879c57029330019734170882a5c
nkomoo <nkomochantele@gmail.com>
2022-07-15 23:20:12 +0100

    Added more images and fixed CSS

--- homepage/app.js
+++ homepage/app.js
@@ -1,12 +1,21 @@
-document.getElementsByClassName("tech").addEventListener("click", function () {
-    body.style.backgroundImage = "url('https://im3.ezgif.com/tmp/ezgif-3-ac8386abf5.gif')";
-});
+
 
 function myFunction() {
     document.body.style.backgroundColor = "#00000";
-    document.body.style.backgroundImage = "url('https://im3.ezgif.com/tmp/ezgif-3-ac8386abf5.gif')";
+    document.body.style.backgroundImage = "url('/gifs/typing-dark.gif')";
     document.body.style.backgroundRepeat = "no-repeat";
     document.body.style.backgroundSize = "contain";
     document.body.style.backgroundPosition = "center";
     document.body.style.backgroundAttachment = "fixed";
 }
+
+
+
+// TO DO List 
+
+
+function myTodo() {
+
+
+}
+
