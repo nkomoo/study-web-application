66ee14ba1d0da958607a4eb8e407a08d0c7b4c13
nkomoo <nkomochantele@gmail.com>
2022-07-18 23:03:32 +0100

    Added CSS Grid for responsiveness and fixed some styling

--- homepage/app.js
+++ homepage/app.js
@@ -2,7 +2,7 @@
 
 function myFunction() {
     document.body.style.backgroundColor = "#00000";
-    document.body.style.backgroundImage = "url('https://im4.ezgif.com/tmp/ezgif-4-2a5dfe2d2f.gif')";
+    document.body.style.backgroundImage = "url('/homepage/Typing-dark.gif')";
     document.body.style.backgroundRepeat = "no-repeat";
     document.body.style.backgroundSize = "contain";
     document.body.style.backgroundPosition = "center";
@@ -19,6 +19,7 @@ function fullScreen() {
         x.requestFullscreen();
     }
 }
+
 
 // pop up so people can share web application
 
